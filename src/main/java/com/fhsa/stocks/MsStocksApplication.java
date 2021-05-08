d128e6befd283dde30d1532fb0f6b524106f8ecf
Fábio H. S. de Almeida <fabio.h.s.almeida@gmail.com>
2021-05-08 19:56:41 -0300

    init

---
+++ src/main/java/com/fhsa/stocks/MsStocksApplication.java
@@ -0,0 +1,13 @@
+package com.fhsa.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fhsa/stocks/MsStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhsa.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
